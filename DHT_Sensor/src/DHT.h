7b97fc958ed961fc25c9df63aa8d0f7e10e8c5a0
offonrynk <nkrumahoffonry@gmail.com>
2020-01-10 23:51:40 +0100

    [Edits]:- clang format changes for DHt and editing base README

--- DHT_Sensor/src/DHT.h
+++ DHT_Sensor/src/DHT.h
@@ -1,49 +1,58 @@
-//heat Index was computed 
-// Using both Rothfusz and Steadman's equations
-// http://www.wpc.ncep.noaa.gov/html/heatindex_equation.shtml
+/** heat Index was computed
+ * Using both Rothfusz and Steadman's equations
+ * http://www.wpc.ncep.noaa.gov/html/heatindex_equation.shtml
 
-//the constructor is set as private following the Singleton design pattern. 
-/* This project uses the BCM NATIVE HARDWARE NUMBERING FOR Setupmode.
-//In main, wiringPiSetupGpio() mode is used */
-//The project should also be runned as root for it to work
-//for test purpose Gpio17 was used, so pin was declared as 17
+ * the constructor is set as private following the Singleton design pattern.
+ * This project uses the BCM NATIVE HARDWARE NUMBERING FOR Setupmode.
+ * In main, wiringPiSetupGpio() mode is used
+ * The project should also be runned as root for it to work
+ * for test purpose Gpio17 was used, so pin was declared as 17
+ */
 
 #ifndef DHT_H
 #define DHT_H
+#endif
 
-//standard includes
+// standard includes
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <math.h>
 #include <iostream>
 
-
-//defines
+// defines
 #define DHT_MAXCOUNT 32000
 #define DHT_PULSES 41
 
+class DHT
+{
+  private:
+    static DHT *_instance;
+    float *temperature;
+    float *humidity;
+    static float t, h;
+    float convertCtoF(float c);
+    float convertFtoC(float f);
+    bool pulse_setter();
+    void read_sensor();
+    static uint8_t data[5];
+    uint8_t laststate;
+    static uint16_t _pin;
+    float start;
 
-class DHT{
-	private: 
-		DHT() {start = 0.0; temperature = &start; humidity = &start;}
-		static DHT * _instance;
-		float *temperature; 
-		float *humidity;
-		static float t, h;
-		float convertCtoF(float c);
-		float convertFtoC(float f);
-		bool pulse_setter();
-		void read_sensor();
-		static uint8_t data[5];
-		uint8_t laststate;
-		static uint16_t _pin;
-		float start;
-	public:
-		static DHT * getInstance(uint16_t pin);
-		float get_temperature();
-		float get_humidity();
-		float get_heatIndex();
+  protected:
+    DHT()
+    {
+        start = 0.0;
+        temperature = &start;
+        humidity = &start;
+    }
+
+  public:
+    static DHT *getInstance(uint16_t pin);
+    float get_temperature();
+    float get_humidity();
+    float get_heatIndex();
 };
 
 #endif
--- DHT_Sensor/src/DHTWrapper.cpp
+++ DHT_Sensor/src/DHTWrapper.cpp
@@ -1,16 +1,19 @@
 #include "DHTWrapper.h"
+#include <sys/wait.h>
 
-DHTWrapper::DHTWrapper(uint16_t pin){
+DHTWrapper::DHTWrapper(uint16_t pin)
+{
     dht = DHT::getInstance(pin);
 }
 
-DHTWrapper::manual_reading(){
-    //Read temperature twice to make sure reading is accurate
-    dht -> get_temperature();
+void DHTWrapper::manual_reading()
+{
+    // Read temperature twice to make sure reading is accurate
+    dht->get_temperature();
     wait(READ_INTERVAL);
-    std::cout<<"Temperature: " << dht -> get_temperature() << (char)167 << "C" << " ";
-    std::cout<<"(Real feel: "<< dht -> get_heatIndex() << (char)167 << "C)" << std::endl;
-    std::cout<<"Humidity: " << dht -> get_humidity() << (char)37 << std::endl;
+    std::cout << "Temperature: " << dht->get_temperature() << (char)167 << "C"
+              << " ";
+    std::cout << "(Real feel: " << dht->get_heatIndex() << (char)167 << "C)"
+              << std::endl;
+    std::cout << "Humidity: " << dht->get_humidity() << (char)37 << std::endl;
 }
-
-
